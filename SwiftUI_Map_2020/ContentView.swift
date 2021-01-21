0f5c4d328d445c244a49c28340998d12afb4e4a8
shun-shun <gondo.syun0711@gmail.com>
2021-01-21 21:50:44 +0900

    Initial Commit

---
+++ SwiftUI_Map_2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Map_2020
+//
+//  Created by しゅん on 2021/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
